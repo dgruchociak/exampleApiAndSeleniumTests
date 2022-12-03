08c3aa869b9c764c49fb9c5acee048e73036d612
dan.gruchciak@gmail.com <dan.gruchciak@gmail.com>
2022-12-03 15:53:40 +0100

    Revert "API scenarios"
    This reverts commit 70c837ceea795c3b9c8109814b39832d8d5f2151.

---
+++ exampleFramework/exampleFramework/PageObjects/GoogleSearchPage.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace exampleFramework.PageObjects
+{
+    public class GoogleSearchPage : BasePage
+    {
+        public GoogleSearchPage(IWebDriver driver) : base(driver){ }
+
+        private IList<IWebElement> productsList => wait.Until(e => driver.FindElements(By.CssSelector("div.mnr-c.pla-unit")));
+
+        public void clickOnFirstProduct()
+        {
+            productsList.ToList().FirstOrDefault().Click();
+        }
+
+        public void clickOnRandomProduct()
+        {
+            getRandomFromList(productsList).Click();
+        }
+
+        private IWebElement getRandomFromList(IList<IWebElement> list)
+        {
+            Random rnd = new Random();
+            int ele = rnd.Next(list.Count);
+            return list[ele];
+        }
+
+        public string getNewWindowTitle()
+        {
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+
+            return driver.Title;
+        }
+    }
+}
